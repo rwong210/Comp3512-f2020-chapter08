dac68594b89b91da90b0b86c97e113aa095cd207
Ryan J. Wong <rwong210@mtroyal.ca>
2020-10-06 11:44:16 -0600

    up to lab08-ex12 q3

--- f2020-chapter08/js/lab08-ex12.js
+++ f2020-chapter08/js/lab08-ex12.js
@@ -1,3 +1,24 @@
 // define functions in this file
 
+const stock1 = {};
+stock1.symbol = "ADBE";
+stock1.price = 286.81;
+console.log(`${stock1.symbol} Open: ${stock1.price} `);
+
+const stock2 = {
+    symbol: "AAPL",
+    name: "Apple Inc. ",
+    prices: [ 200, 198, 202, 197, 203 ],
+    location: {
+        address: "One Apple Park Way",
+        city: "Cupertino"
+    }
+}
+console.log(`${stock2.name} Address: ${stock2.location.address}`);
+
+console.log(stock2);
+console.log(stock2['name']);
+console.log(stock2['location'].address);
+console.log(stock2['location']['city']);
+
 
--- f2020-chapter08/js/lab08-test01.js
+++ f2020-chapter08/js/lab08-test01.js
@@ -1,8 +1,30 @@
 /* add code below this */
 
-let input = prompt("Enter bill total: ");
-let bill = input;
-const rate = 0.1;
-let tip = bill * rate;
+//let input = prompt("Enter bill total: ");
+//let bill = input;
+//const rate = 0.1;
+//let tip = bill * rate;
 
-isNaN(bill) ? console.log("This is not a valid number.") : console.log('For bill of ' + bill + ' the tip should be $' + tip);
+// isNaN(bill) ? console.log("This is not a valid number.") : console.log('For bill of ' + bill + ' the tip should be $' + tip);
+
+const billTotals = [50, 150, 20, 500];
+
+const tips = new Array();
+
+let tip;
+
+for (let bill of billTotals) {
+    if (bill > 75)
+        tip = bill * 0.1;
+
+    else if (bill <= 75 && bill >= 30)
+        tip = bill * 0.2;
+
+    else if (bill < 30)
+        tip = bill * 0.3;
+
+    tips.push(tip);
+}
+for (let i = 0; i < billTotals.length; i++){
+    console.log(`For bill of \$${billTotals[i]} the tip should be \$${tips[i]}`);
+};
