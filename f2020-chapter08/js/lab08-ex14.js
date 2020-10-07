054d7fd71c784552805371af8792022e8ae1ba03
Ryan J. Wong <rwong210@mtroyal.ca>
2020-10-06 21:14:14 -0600

    lab08-test04 start

--- f2020-chapter08/js/lab08-ex14.js
+++ f2020-chapter08/js/lab08-ex14.js
@@ -1,4 +1,13 @@
 /* code goes here */
 
+console.log(content[0].title);
+console.log(typeof content);
+
+const photos =JSON.parse(content);
+
+console.log(typeof photos);
+console.log(photos[0].title);
+console.log(photos);
+
 
 
