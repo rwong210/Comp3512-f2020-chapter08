2cf26c582cc3d0450accfa6309d1662085ac7327
Ryan J. Wong <rwong210@mtroyal.ca>
2020-10-06 23:52:23 -0600

    test04 q 4

--- f2020-chapter08/js/lab08-test04.js
+++ f2020-chapter08/js/lab08-test04.js
@@ -1,64 +1,78 @@
-
 // convert comma-separated values into an array
-
+const countries = csv.split(',');
+console.log(countries);
 
 // convert array into delimited string
-
+let foo = countries.join();
+console.log(foo);
 
 // check if these are arrays
-
+console.log(Array.isArray(countries));
+console.log(Array.isArray(csv));
 
 //  sort a simple array 
-
+console.log(countries.sort());
 
 // reverse the sort
-
+console.log(countries.reverse());
 
 // remove the first element
-
+console.log(countries.shift());
 
 // remove the last element
-
+console.log(countries.pop());
 
 // add new elements to the front of the array
-
+console.log(countries.unshift("Mexico", "Sweden"));
 
 // search for element
-
+console.log(countries.includes("Germany"));
 
 // search for element index
-
+console.log(countries.indexOf("Germany"));
 
 // make a new array by extracting from another array
-
-
-console.log('---------------------');
+const countries2 = countries.splice(0, 3);
+console.log(countries2);
 
 // there are other array methods which you will learn in Ch 10 */
 
 
 // use a loop to output all cities whose continent=="NA"
-
-
-console.log('---------------------');
+for (let c of cities) {
+   if (c.continent == "NA") {
+      console.log(c.city);
+   }
+}
 
 // use a loop to output gallery names whose country=="USA"
-
-
-console.log('---------------------');
+for (let g of galleries) {
+   if (g.location.country == "USA") {
+      console.log(g.name);
+   }
+}
 
 // convert JSON colorsAsString to js object
 const colors = JSON.parse(colorsAsString);
 
 // use a loop to output color name if luminance < 75
-
-
-console.log('---------------------');
+for (let c of colors){
+   if (c.luminance < 75){
+      console.log(c.name);
+   }
+}
 
 // use two nested loops - outer: loop thru colors
-
-
-console.log('---------------------');
+for (let c of colors){
+   console.log((c.name));
+   let x;
+   for(i=0; i<c.rgb.length; i++){
+      x += (c.rgb[i]);
+      console.log(x);
+   }
+  
+      
+   }
 
 /* use a loop output using document.write a unordered
    list of links to the galleries in the galleries array.
