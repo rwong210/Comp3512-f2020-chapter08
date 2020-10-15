24307e438058f2e8a6db7b2cc61db25d95ec183d
Ryan J. Wong <rwong210@mtroyal.ca>
2020-10-15 16:09:24 -0600

    ex17 done

--- f2020-chapter08/js/lab08-ex17.js
+++ f2020-chapter08/js/lab08-ex17.js
@@ -1,2 +1,21 @@
 /* code goes here */
 
+const isCanadian = true;
+
+
+
+function calculateTax(amount, tax) {
+    return amount + tax();
+}
+
+function calculateTotal(price, quantity) {
+    let amount = price + quantity;
+    return amount + calculateTax(amount, function () {
+        if (isCanadian) {
+            return 0.05;
+        } else {
+            return 0.0;
+        }
+    });
+}
+
