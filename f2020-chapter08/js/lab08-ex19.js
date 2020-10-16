3f34d1c39d5ce5bd41d25b841b3bdf78f1e41ee1
Ryan J. Wong <rwong210@mtroyal.ca>
2020-10-15 22:05:04 -0600

    start of test#6

--- f2020-chapter08/js/lab08-ex19.js
+++ f2020-chapter08/js/lab08-ex19.js
@@ -1,32 +1,58 @@
-const f1 = function() {
+const f1 = function () {
    return "data from f1";
 }
-const f2 = function(s) {
+const f2 = function (s) {
    return s;
 }
-const f3 = function(a,b) {
-   return "function 2 result=" + (a+b);
+const f3 = function (a, b) {
+   return "function 2 result=" + (a + b);
 }
-const f4 = function(a,b) {
+const f4 = function (a, b) {
    let s = "function 3 result=";
-   let product = a*b;
+   let product = a * b;
    let msg = s + product;
    return msg;
 }
-const f5 = function(a,b, fn) {
-   let msg = f4(a,b) + fn();
+const f5 = function (a, b, fn) {
+   let msg = f4(a, b) + fn();
    return msg;
 }
 
-console.log( f1() );
-console.log( f2('hello') );
-console.log( f3(4,5) );
-console.log( f4(5,6) );
-console.log( f5(7,8, function() {
-                      return " param f4"
-                   }) );
+console.log(f1());
+console.log(f2('hello'));
+console.log(f3(4, 5));
+console.log(f4(5, 6));
+console.log(f5(7, 8, function () {
+   return " param f4"
+}));
 
 console.log('---------------------');
 
 /* now define arrow function equivalents */
 
+const f1a = () => "data from 1a";
+console.log(f1a());
+const f2a = s => s;
+console.log(f2a('hello f2a'));
+
+const f3a = (a, b) => {
+   return "function 3a result=" + (a + b);
+};
+console.log(f3a(4, 5));
+
+const f4a = (a, b) => {
+   let s = "function 4a result=";
+   let product = a * b;
+   let msg = s + product;
+   return msg;
+};
+
+console.log(f4a(6, 7));
+
+const f5a = (a, b, fn) => {
+   let msg = f4(a, b) + fn();
+   return msg;
+}
+console.log(f5a(7, 8, () => {
+   return " param f5a"
+}));
--- f2020-chapter08/js/lab08-ex20.js
+++ f2020-chapter08/js/lab08-ex20.js
@@ -1,4 +1,3 @@
-
 /* To refresh your memory, here are two complex objects created using
    object literal syntax. If we have many objects with the same structure,
    this is a time- and space-consuming way of doing so.
@@ -24,3 +23,32 @@ const google = {
 
 /* after this add your own code */
 
+function Company(symbol, name, address, city) {
+   this.symbol = symbol;
+   this.name = name;
+   this.location = {};
+   this.location.address = address;
+   this.location.city = city;
+   this.output = function() {
+   console.log(`${this.symbol} - ${this.name}`);
+}
+}
+
+const apple2 = new Company("AAPL", "Apple Inc.", "One Apple Park Way", "Cupertino");
+
+const google2 = new Company("GOOG", "Alphabet Inc.", "1600 Amphitheatre Parkway", "Mountain View");
+
+console.log(apple2);
+console.log(apple2.location.address);
+
+apple2.output();
+
+const companies = [
+   new Company("AAPL", "Apple Inc.", "One Apple Park Way", "Cupertino"),
+   new Company("PYPL", "PayPal Holdings Inc.", "2211 N. 1st St.", "San Jose"),
+   new Company("MSFT", "Microsoft Corporation", "One Microsoft Way", "Redmond")
+];
+
+for (let c of companies){
+   c.output();
+}
