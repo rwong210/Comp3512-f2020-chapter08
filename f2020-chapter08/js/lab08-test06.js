4a12b217c9f6cd53d10c4a41c22e721032ab3472
Ryan J. Wong <rwong210@mtroyal.ca>
2020-11-13 10:35:33 -0700

    fixed AcctCalculations Function, now working

--- f2020-chapter08/js/lab08-test06.js
+++ f2020-chapter08/js/lab08-test06.js
@@ -16,17 +16,18 @@ function Company(symbol, companyName, exchange, industry, website, description, 
 }
 
 function AcctCalculations(Company) {
-    this.marketCap50 = function () {
-        return currency(day50MovingAvg * sharesOutstanding);
+
+    this.marketCap50 = function (Company) {
+        return currency(Company.stats.day50MovingAvg * Company.stats.sharesOutstanding);
     };
-    this.marketCap200 = function () {
-        return currency(day200MovingAvg * sharesOutstanding);
+    this.marketCap200 = function (Company) {
+        return currency(Company.stats.day200MovingAvg * Company.stats.sharesOutstanding);
     };
-    this.revenue = function () {
-        return currency(operatingRevenue - costOfRevenue);
+    this.revenue = function (Company) {
+        return currency(Company.stats.operatingRevenue - Company.stats.costOfRevenue);
     };
-    this.shareHolderEquity = function () {
-        return currency(totalAssets - totalLiabilities);
+    this.shareHolderEquity = function (Company) {
+        return currency(Company.stats.totalAssets - Company.stats.totalLiabilities);
     };
     const currency = function (num) {
         return new Intl.NumberFormat("en-us", {
@@ -36,14 +37,15 @@ function AcctCalculations(Company) {
     };
 }
 
+
 function outputCard(Company) {
     document.write(`<article class="card"><h2>${Company.symbol}-${Company.companyName}</h2><div>       
     <p>Share Price (50day avg): <span>$${Company.stats.day50MovingAvg}</span></p>
     <p>Share Price (200day avg): <span>$${Company.stats.day200MovingAvg}</span></p>
-    <p>Market Cap (50day avg): <span>$${AcctCalculations.marketCap50}</span></p>
-    <p>Market Cap (200day avg): <span>$${AcctCalculations.marketCap200}</span></p>
-    <p>Net Revenue: <span>$${AcctCalculations.revenue}</span></p>
-    <p>Shareholder Equity: <span>$${AcctCalculations.shareHolderEquity}</span></p>
+    <p>Market Cap (50day avg): <span>${new AcctCalculations().marketCap50(Company)}</span></p>
+    <p>Market Cap (200day avg): <span>${new AcctCalculations().marketCap200(Company)}</span></p>
+    <p>Net Revenue: <span>${new AcctCalculations().revenue(Company)}</span></p>
+    <p>Shareholder Equity: <span>${new AcctCalculations().shareHolderEquity(Company)}</span></p>
   </div>
   <footer>
       <small>Technology</small> 
